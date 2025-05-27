87d88e8c6c406d8e821bd489ed7816f283bb3245
Luis Demetrio Perez Ortiz <luisdemetrio816@gmail.com>
2025-05-27 14:44:19 -0600

    feat: add responsive images to Hero section
    - Added different images in the Hero section to display based on screen size for better responsiveness and visual experience.

--- src/components/Hero/Highlight.jsx
+++ src/components/Hero/Highlight.jsx
@@ -3,7 +3,7 @@ import { motion, scale } from 'framer-motion';
 
 import CustomDiv from '../CustomDiv/CustomDiv';
 import CustomImage from '../CustomImage/CustomImage';
-import {ViewportContext} from '../../context/ViewportContext';
+import { ViewportContext } from '../../context/ViewportContext';
 import CustomParagraph from '../CustomParagraph/CustomParagraph';
 
 const MotionCustomDiv = motion.create(CustomDiv);
@@ -11,7 +11,7 @@ const MotionCustomParagraph = motion.create(CustomParagraph);
 
 const Highlight = () => {
 
-    const {viewportWidth} = useContext(ViewportContext);
+    const { viewportWidth } = useContext(ViewportContext);
     // console.log(viewportWidth >= 768)
     return (
         <div className="hero__highlight">
@@ -19,32 +19,46 @@ const Highlight = () => {
                 className="hero__highlight-img-container"
 
                 variants={{
-                    hidden: { opacity: 0, scale:.5 },
-                    visible: {opacity: 1, scale: 1, y:viewportWidth >= 768?'-50%':'0%'},
+                    hidden: { opacity: 0, scale: .5 },
+                    visible: { opacity: 1, scale: 1, y: viewportWidth >= 768 ? '-50%' : '0%' },
                 }}
                 initial="hidden"
                 whileInView="visible"
                 viewport={{ once: true, amount: 0.2 }}
                 transition={{ duration: 1.4, ease: "easeOut" }}
             >
-                <CustomImage
-                    src="/starbucks-hero.webp"
-                    className="hero__highlight-img"
-                    alt="Highlight product"
-                    isLazyLoadingEnabled={false}
-                />
+                <picture>
+                    <source
+                        media="(max-width: 767px)"
+                        srcSet="/starbucks-hero-250.webp"
+                        width="400"
+                        height="250"
+                    />
+                    <source
+                        media="(min-width: 768px)"
+                        srcSet="/starbucks-hero.webp"
+                        width="800"
+                        height="500"
+                    />
+                    <CustomImage
+                        src="/starbucks-hero.webp"
+                        className="hero__highlight-img"
+                        alt="Highlight product"
+                        isLazyLoadingEnabled={false}
+                    />
+                </picture>
             </MotionCustomDiv>
-            <MotionCustomDiv 
+            <MotionCustomDiv
                 className="hero__highlight-info"
-                initial={{opacity:0, x: 300, rotate:viewportWidth >= 768? 0:180}}
-                whileInView={{ opacity:1, x: 0, rotate:viewportWidth >= 768? 180:0}}
+                initial={{ opacity: 0, x: 300, rotate: viewportWidth >= 768 ? 0 : 180 }}
+                whileInView={{ opacity: 1, x: 0, rotate: viewportWidth >= 768 ? 180 : 0 }}
                 transition={{ duration: 1.2, ease: 'easeOut', }}
                 viewport={{ once: true, amount: 0.2 }}
             >
-                <MotionCustomParagraph 
+                <MotionCustomParagraph
                     className="hero__highlight-title hero__highlight-title--border-white"
-                    initial={{opacity:0, y: 200}}
-                    whileInView={{ opacity:1, y: 0}}
+                    initial={{ opacity: 0, y: 200 }}
+                    whileInView={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1.2, ease: 'easeOut', }}
                     viewport={{ once: true, amount: 0.2 }}
                 >
@@ -52,8 +66,8 @@ const Highlight = () => {
                 </MotionCustomParagraph>
                 <MotionCustomParagraph
                     className="hero__highlight-title hero__highlight-title--color-white"
-                    initial={{opacity:0, y: -200}}
-                    whileInView={{ opacity:1, y: 0}}
+                    initial={{ opacity: 0, y: -200 }}
+                    whileInView={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1, ease: 'easeOut', }}
                     viewport={{ once: true, amount: 0.2 }}
                 >
--- src/components/Hero/ProductCard.jsx
+++ src/components/Hero/ProductCard.jsx
@@ -3,7 +3,7 @@ const ProductCard = ({img,text,alt, ...props}) => {
     return (
         <li className='hero__product' {...props}>
             <div className='hero__product-container-img'>
-                <CustomImage src={img} alt={text} isLazyLoadingEnabled={false} className='hero__product-img'/>
+                <CustomImage src={img} alt={''} isLazyLoadingEnabled={false} className='hero__product-img'/>
             </div>
             <div className='hero__product-description'>
                 <p className='hero__product-text'>{text}</p>
